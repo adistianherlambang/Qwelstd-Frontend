a5fde35787003d36449f79efb558b7199ab5439c
Adistian Herlambang <adistian59@gmail.com>
2026-04-07 15:19:33 +0700

    button Hover

--- src/components/button hover/buttonHover.jsx
+++ src/components/button hover/buttonHover.jsx
@@ -6,21 +6,52 @@ import styles from "./style.module.css"
 export default function ButtonHover( {children} ) {
 
   const text = String(children).split("")
+  const [hover, setHover] = useState(false)
   
   return (
-    <div className={styles.container}>
+    <div
+      className={styles.container}
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
       {/* {text.map((char, index) => <div>{char} + {index}</div>)} */}
       <div className={styles.textWrapper}>
         {text.map((char, index) =>
           <p
             className={styles.text}
             key={index}
+            style={{
+              display: "inline-block",
+              transition: "transform 0.3s ease",
+              transitionDelay: `${index*20}ms`,
+              transform: hover ? "translateY(-30px)" : "translateY(0)"
+            }}
           >
             {char === " " ? "\u00A0" : char}
           </p>
         )}
       </div>
-      <div className={styles.box}></div>
+      <div className={styles.textWrapper2}>
+        {text.map((char, index) =>
+          <p
+            className={styles.text}
+            key={index}
+            style={{
+              display: "inline-block",
+              transition: "transform 0.3s ease",
+              transitionDelay: `${index*20}ms`,
+              transform: hover ? "translateY(0px)" : "translateY(30px)"
+            }}
+          >
+            {char === " " ? "\u00A0" : char}
+          </p>
+        )}
+      </div>
+      <div className={styles.box} style={{backgroundColor: hover ? "white" : "#EE3E75"}}>
+        <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0.75 5.25L12.75 5.25M12.75 5.25L8.25 9.75M12.75 5.25L8.25 0.75" stroke={hover ? "black" : "white"} stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </div>
     </div>
   )
 }
